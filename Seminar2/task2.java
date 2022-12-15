1136f6f33f5f35cfbc03fcfa9e051c0405315580
Karina Babakova <taytynovak@mail.ru>
2022-12-15 21:29:25 +0700

    task2

--- Seminar2/task2.java
+++ Seminar2/task2.java
@@ -2,8 +2,13 @@ package Seminar2;
 
 import java.util.Scanner;
 
-//Напишите программу, чтобы найти вхождение в строке (содержащей все символы другой строки).
-public class task1 {
+//Напишите программу, чтобы проверить, являются ли две данные строки вращением друг друга (вхождение в обратном порядке).
+public class task2 {
+    public static String reverseString(String str){  
+        StringBuilder sb=new StringBuilder(str);  
+        sb.reverse();  
+        return sb.toString();  
+    }  
     public static void main(String[] args) {
         System.out.println("Hi my world!");
         Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
@@ -14,7 +19,7 @@ public class task1 {
         System.out.println("You have entered: "+str);      
         System.out.println("Trying find "+strTo);  
         System.out.println("Result");  
-        System.out.println(str.toLowerCase().indexOf(strTo.toLowerCase(), 0));
+        System.out.println(str.equals(reverseString(strTo)));
         sc.close();
     }
 }

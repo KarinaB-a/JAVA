4b951214e71c4377e2bbb7ed063bfe8e786d74be
Karina Babakova <taytynovak@mail.ru>
2022-12-27 22:02:04 +0700

    task1,2

--- Seminar4/task1.java
+++ Seminar4/task1.java
@@ -1,15 +1,26 @@
 //Написать приложение для ввода ФИО, возраста и пола пользователей. Данные хранить в разных списках.
-
-
+//Добавить методы для сортировки пользователей по возрасту полу и первой букве фамилии.
 package Seminar4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class task1 {
+    public static void sortByAge(ArrayList<List<String>> db){
+        db.sort(Comparator.comparing(x -> Integer.parseInt(x.get(1))));
 
+    }
+    public static void sortByFIO(ArrayList<List<String>> db){
+        db.sort(Comparator.comparing(x -> x.get(0).charAt(0)));
+
+    }
+    public static void sortBySex(ArrayList<List<String>> db){
+        db.sort(Comparator.comparing(x -> x.get(2)));
+
+    }
     public static List<String> createUser(Scanner sc) {
         System.out.print("Введите ФИО: ");
         String FIO = sc.nextLine();
@@ -31,7 +42,7 @@ public class task1 {
         Scanner sc = new Scanner(System.in);
         ArrayList<List<String>> db = new ArrayList<List<String>>();
         while(true) {
-            System.out.print("Выберите команду:\n1 - ввести пользователя\n2 - вывести базу пользователей\n3 - выйти\n");
+            System.out.print("Выберите команду:\n1 - ввести пользователя\n2 - вывести базу пользователей\n3 - выйти\n4 - сортировка по ФИО\n5 - сортировка по возрасту\n6 - сортировка по полу\n");
             String command = sc.nextLine().trim();
 
             if(command.equals("1")){
@@ -43,11 +54,21 @@ public class task1 {
             else if(command.equals("3")){
                 break;
             }
+            else if(command.equals("4")){
+                sortByFIO(db);
+                printDB(db);
+            }
+            else if(command.equals("5")){
+                sortByAge(db);
+                printDB(db);
+            }
+            else if(command.equals("6")){
+                sortBySex(db);
+                printDB(db);
+            }
         }
         sc.close();
         
-
-
     }
 }
         

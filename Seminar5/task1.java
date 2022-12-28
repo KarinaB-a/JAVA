4efc547e68d89b7b9bccac8341cae13e64be1ec6
Karina Babakova <taytynovak@mail.ru>
2022-12-28 20:54:17 +0700

    task1-5

--- Seminar5/task1.java
+++ Seminar5/task1.java
@@ -1,11 +1,15 @@
 package Seminar5;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 //Создать словарь HashMap. Обобщение <Integer, String>.
 //Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключ, если не было!)
 //Изменить значения сделав пол большой буквой.
 //Пройти по коллекции и вывести значения в формате Фамилия инициалы "Иванов И.И."
+//*Сортировать значения по возрасту и вывести отсортированную коллекция как в четвёртом пункте.
 public class task1 {
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>();
@@ -21,5 +25,12 @@ public class task1 {
             String[] a = map.get(key).split(" ");
             System.out.printf("%s %s.%s.\n", a[0], a[1].substring(0, 1), a[2].substring(0, 1));
         }
+        List<String> values = new ArrayList<>(map.values());
+        values.sort(Comparator.comparing(x -> Integer.parseInt(x.split(" ")[3])));
+        for (String i : values) {
+            String[] a = i.split(" ");
+            System.out.printf("%s %s.%s. %s %s\n", a[0], a[1].substring(0, 1), a[2].substring(0, 1), a[3], a[4]);
+        }
+
     }
 }

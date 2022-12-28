4f283f0289763aa06ca06a1f380321a451f8dd13
Karina Babakova <taytynovak@mail.ru>
2022-12-28 20:30:28 +0700

    task3

--- Seminar5/task1.java
+++ Seminar5/task1.java
@@ -3,7 +3,8 @@ package Seminar5;
 import java.util.HashMap;
 
 //Создать словарь HashMap. Обобщение <Integer, String>.
-//Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключь, если не было!)
+//Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключ, если не было!)
+//Изменить значения сделав пол большой буквой.
 public class task1 {
    public static void main(String[] args) {
     HashMap<Integer, String> map = new HashMap<>();
@@ -12,5 +13,11 @@ public class task1 {
     map.put(3, "Иванов Василий Иванович 30 м");
     map.put(4, "Иванова Василиса Ивановна 25 ж");
     map.put(5, "Иванов Евгений Иванович 25 м");
+    System.out.println(map);
+    map.put(1, "Иванов Иван Иванович 28 М");
+    System.out.println(map);
+
+
    } 
+   
 }

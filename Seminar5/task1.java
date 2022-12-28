02f0bc18eb68485ed87e69b0e312fbb170f17de0
Karina Babakova <taytynovak@mail.ru>
2022-12-28 20:18:16 +0700

    task1

---
+++ Seminar5/task1.java
@@ -0,0 +1,10 @@
+package Seminar5;
+
+import java.util.HashMap;
+
+//Создать словарь HashMap. Обобщение <Integer, String>.
+public class task1 {
+   public static void main(String[] args) {
+    HashMap<Integer, String> map = new HashMap<>();
+   } 
+}

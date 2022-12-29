ddb97d0dd8a5b3a4d814c3b287b782619d2d4b98
Karina Babakova <taytynovak@mail.ru>
2022-12-29 21:30:16 +0700

    task2

--- Seminar6/task1.java
+++ Seminar6/task1.java
@@ -3,13 +3,15 @@ package Seminar6;
 import java.util.HashSet;
 
 //Объявить и инициализировать экземпляр класса HashSet. Обобщение String.
+//Добавить пять элементов в множество.
 public class task1 {
     public static void main(String[] args) {
         HashSet<String> map = new HashSet<>();
-        map.put(1, "Иванов Иван Иванович 28 м");
-        map.put(2, "Иванов Петр Иванович 29 м");
-        map.put(3, "Иванов Василий Иванович 30 м");
-        map.put(4, "Иванова Василиса Ивановна 25 ж");
-        map.put(5, "Иванов Евгений Иванович 25 м");
+        map.add("Иванов Иван Иванович 28 м");
+        map.add("Иванов Петр Иванович 29 м");
+        map.add("Иванов Василий Иванович 30 м");
+        map.add("Иванова Василиса Ивановна 25 ж");
+        map.add("Иванов Евгений Иванович 25 м");
         System.out.println(map);
+    }
 }

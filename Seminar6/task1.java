1034eff78d20fbe1ed813fe5d1d5d81bfc372f50
Karina Babakova <taytynovak@mail.ru>
2022-12-29 21:55:22 +0700

    task3-4

--- Seminar6/task1.java
+++ Seminar6/task1.java
@@ -1,17 +1,36 @@
 package Seminar6;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 
 //Объявить и инициализировать экземпляр класса HashSet. Обобщение String.
 //Добавить пять элементов в множество.
+//Отсортировать значения используя итератор.
+//Отсортированный результат сохранить в LinkedHashSet
 public class task1 {
     public static void main(String[] args) {
-        HashSet<String> map = new HashSet<>();
-        map.add("Иванов Иван Иванович 28 м");
-        map.add("Иванов Петр Иванович 29 м");
-        map.add("Иванов Василий Иванович 30 м");
-        map.add("Иванова Василиса Ивановна 25 ж");
-        map.add("Иванов Евгений Иванович 25 м");
-        System.out.println(map);
+        HashSet<String> set = new HashSet<>();
+        set.add("28");
+        set.add("29");
+        set.add("30");
+        set.add("25");
+        set.add("25");
+        System.out.println(set);
+        LinkedHashSet<String> sorted = new LinkedHashSet<>();
+        while (set.size() > 0) {
+            Iterator<String> i = set.iterator();
+            String s = i.next();
+            while(i.hasNext()) {
+                String v = i.next();
+                if(v.compareTo(s) > 0) {
+                    s = v;
+                }
+            }
+            set.remove(s);
+            sorted.add(s);
+        }
+        System.out.println(sorted);
+ 
     }
 }
